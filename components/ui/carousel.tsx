44527f224e7184edea559fc3570926d609f911d0
백경희 <kkunghi@naver.com>
2026-09-12 07:45:57 +0900

    Fix React hook lint errors

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -95,12 +95,17 @@ function Carousel({
 
   React.useEffect(() => {
     if (!api) return
-    onSelect(api)
+    let active = true
+    queueMicrotask(() => {
+      if (active) onSelect(api)
+    })
     api.on("reInit", onSelect)
     api.on("select", onSelect)
 
     return () => {
-      api?.off("select", onSelect)
+      active = false
+      api.off("reInit", onSelect)
+      api.off("select", onSelect)
     }
   }, [api, onSelect])
 
